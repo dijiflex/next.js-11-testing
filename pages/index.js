3aedf6e24d4cd2fc5f074cc954a0ea8fc9111206
dijiflex <dijiflex@gmail.com>
2021-06-19 16:00:54 +0300

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
-
+import Link from 'next/link';
+ 
 export default function Home() {
   return (
     <div className="container">
@@ -9,9 +10,12 @@ export default function Home() {
       </Head>
 
       <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+      <h1 className="title">
+  Read{' '}
+  <Link href="/posts/first-post">
+    <a>this page!</a>
+  </Link>
+</h1>
 
         <p className="description">
           Get started by editing <code>pages/index.js</code>
