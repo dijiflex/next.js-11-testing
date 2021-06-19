0717cd81632c190ac8e267343a21dee0433cac32
dijiflex <dijiflex@gmail.com>
2021-06-19 18:14:34 +0300

    Polished post page

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,8 +1,10 @@
 import Layout from '../../components/layout'
-import { getAllPostIds, getPostData } from '../../lib/posts'
+import { getAllPostIds, getPostData } from '../../lib/posts';
+import Head from 'next/head'
+
 
 export async function getStaticProps({ params }) {
-  const postData = getPostData(params.id)
+  const postData = await getPostData(params.id)
   return {
     props: {
       postData
@@ -14,11 +16,16 @@ export async function getStaticProps({ params }) {
 export default function Post({ postData }) {
     return (
       <Layout>
+        <Head>
+        <title>{postData.title}</title>
+      </Head> 
         {postData.title}
         <br />
         {postData.id}
         <br />
         {postData.date}
+        <br />
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
       </Layout>
     )
   }
